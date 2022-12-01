14ef1bf6da779a6e9749870ef252ccb794c40107
ru5s <hellperock@gmail.com>
2022-12-01 11:30:40 +0500

    .

--- sc_TestInterview/ViewController.swift
+++ sc_TestInterview/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let semaphore = DispatchSemaphore(value: 1)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+//        var buildings = ["Chrysler Buildings",  "Empire State Buildings", "Burj Khalifa", "Federation Tower", "Lakhta Center" ]
+//
+//        buildings.sort {
+//        $0 < $1
+//         }
+//
+//        print(buildings)
+        
+//        let queue = DispatchQueue.main
+//        queue.sync {
+//            print("ef")
+//        }
+        let url = URL(string: "www.mail.ru", relativeTo: URL(string: "/main"))
+        print(url)
+        
     }
 
 
