a16e95e0005237ad11248bd56d1792278f99651d
ru5s <hellperock@gmail.com>
2022-12-01 10:10:38 +0500

    Initial Commit

---
+++ sc_TestInterview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sc_TestInterview
+//
+//  Created by Ruslan Ismailov on 01/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
